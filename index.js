45a1d1df0dbcd33ce1e3f24b992c56752c614b6c
wellfrog <wellfrog16@gmail.com>
2022-08-10 23:10:36 +0800

    '初始化'

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        'stylelint-config-standard-scss',
+        "stylelint-config-html/vue",
+        'stylelint-config-recommended-vue/scss',
+        "stylelint-config-recess-order",
+    ],
+    rules: {
+        indentation: 4, // 4个空格
+        'no-eol-whitespace': [true, { ignore: ['empty-lines'] }],
+        'selector-class-pattern': '^[a-z]+[a-z\\d-_]*[a-z\\d]$', // class名字规则
+        'selector-id-pattern': '^[a-zA-Z\\d]+$', // id名字规则
+        'max-nesting-depth': 3, // 最大嵌套深度
+        'max-empty-lines': null, // 如果设置数字，vue文件自动修复有问题
+        'string-quotes': 'single', // 单引号
+        'function-url-quotes': 'always', // 单引号
+        'property-no-unknown': [true, { ignoreProperties: ['return'] }], // 自定义函数用，忽略return
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+        'selector-list-comma-newline-after': 'always-multi-line', // 不允许多行
+        'no-descending-specificity': null, // 选择器顺序，此功能bug太多
+        // ---
+        'scss/at-extend-no-missing-placeholder': null,
+    },
+};
